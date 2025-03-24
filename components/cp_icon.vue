e2eeb3d9d9a415c4dc30ddf57ad079d09f1aa236
siannun <nunsian2024@gmail.com>
2025-03-24 10:00:51 +0700

    add icon module

---
+++ components/cp_icon.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+  name: {
+    type: String,
+    required: true,
+  },
+  iconset: {
+    type: String,
+    default: "lucide",
+  },
+  size: {
+    type: String,
+    default: "w-6 h-6",
+  },
+  color: {
+    type: String,
+    default: "text-gray-600",
+  },
+});
+</script>
+<template>
+  <div>
+    <Icon
+      :name="`${props.iconset}:${props.name}`"
+      :size="props.size"
+      :width="`${props.size}px`"
+      :height="`${props.size}px`"
+      :color="props.color"
+    />
+  </div>
+</template>
